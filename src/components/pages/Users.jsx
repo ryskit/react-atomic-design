315367e8eaf93468a4bfa3b34be44f62d0021e91
ryskit <ryosukekida.71@gmail.com>
2022-10-23 07:07:09 +0900

    :hammer: 不要なstateのバケツリレーを削除

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
 import { SearchInput } from "../molecules/Searchinput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -19,15 +18,13 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
   return (
     <SContainer>
       <h2>Users</h2>
       <SearchInput />
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={users.id} user={user} isAdmin={isAdmin} />
+          <UserCard key={users.id} user={user} />
         ))}
       </SUserArea>
     </SContainer>
